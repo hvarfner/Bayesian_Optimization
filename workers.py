0b67d9455abaf61997768a8888978ccacc00bcc1
hvarfner <58733990+hvarfner@users.noreply.github.com>
2020-04-11 21:23:10 +0200

    attempting concurrency

--- workers.py
+++ workers.py
@@ -1,5 +1,10 @@
 from time import sleep
-
+import numpy as np
 def worker(w, t):
     sleep(n)
     return w, t
+
+def test_function(c_array, i):
+    zZz = np.random.randint(10)
+    time.sleep(zZz)
+    c_array[i] = zZz
